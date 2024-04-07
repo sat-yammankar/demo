51fb027b9715266d4a63876e8974d11143bfaac7
sat-yammankar <166303429+sat-yammankar@users.noreply.github.com>
2024-04-07 21:46:18 +0530

    Update satyam.js

--- satyam.js
+++ satyam.js
@@ -2,6 +2,6 @@ const name = 'Satyam mankar';
 const age = 23;
 
 // Using template literals
-console.log('My name is ${name} and I am ${age} years old.');
+console.log(`My name is ${name} and I am ${age} years old.`);
 
 // Output: My name is Alice and I am  22 years old.
